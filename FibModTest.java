26b817cefac9602ca6c95b9a281116feb8a0dd67
Jacob Liou <jacob.c.liou@gmail.com>
2017-02-02 18:47:58 -0600

    Update FibModTest.java

--- FibModTest.java
+++ FibModTest.java
@@ -13,22 +13,33 @@ import java.lang.Math;
  */
 public class FibModTest {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+public static void main(String[] args)
+  {
+    Scanner in = new Scanner(System.in);
         int f0,f1,f2,m;
         f1 = 1;
         f0 =1;
         f2 = 0;
         System.out.println("Enter a value of m: ");
-        m = in.nextInt();
+    m=4;    
+    int period =0;
+    int power = (int)Math.pow(2,m);
+    //m = in.nextInt();
     System.out.print(f0 + " "+f1+" ");
-        for(int i=0; i<15; i++)
-{
-        f2 = f1+f0;
-        f0 = f1;
-        f1 = f2;
-        System.out.print((f2 % (int)Math.pow(2,m)) + " ");
-}
-    }
-    
+        for(int i=0; i<15; i--)
+        {
+            if(f0%power == 1 && f1%power == 1 && i!=0)
+            {
+              System.out.println("THE PERIOD: " + period);
+              i = 15;
+              break;
+            }
+            f2 = f1+f0;
+            f0 = f1;
+            f1 = f2;
+
+//        System.out.print(f2 % power + " ");
+          period++ ;
+        }
+  }
 }
