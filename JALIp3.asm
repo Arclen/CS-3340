82fc7556186d89365db48baeda8718272fb77557
Jacob Liou <jacob.c.liou@gmail.com>
2017-03-01 12:46:14 -0600

    I should probably switch majors

--- JALIp3.asm
+++ JALIp3.asm
@@ -1,6 +1,7 @@
 .data
 	promptn: .asciiz "\nEnter the value of n: "
 	promptk: .asciiz "Enter the value of k: "
+	debug: .asciiz "\nTEST\n"
 .text
 
 start:	
@@ -21,11 +22,12 @@ start:
 	li $v0, 5
 	syscall
 	move $t1, $v0 # Store k into $t1
-	move $a0, $t0 # Store k into $t1
+	move $a2, $t0 # Store k into $t1
 	move $a1, $t1 # Store k into $t1
 	beq $t0, $zero, exit #if n and k are both zero, exit the program
 continue:
-	jal comb
+	li $v1, 0
+	j combo
 	
 	li $v0, 1
 	move $a0, $v1
@@ -38,76 +40,36 @@ exit:
 	
 	
 	
-	
-	comb:
-sub $sp, $sp, 16
-sw $ra, 0($sp)
-sw $s0, 4($sp)
-sw $a0, 8($sp)
-sw $a1, 12($sp)
-jal fact
-move $s0, $v1
-lw $a0, 12($sp)
-jal fact
-div $s0, $s0, $v1
-lw $a0, 8($sp)
-lw $a1, 12($sp)
-sub $a0, $a0, $a1
-jal fact
-div $s0, $s0, $v0
-move $v1, $s0
-lw $ra, 0($sp)
-lw $s0, 4($sp)
-addi $sp, $sp, 16
-jr $ra
+
 combo:
-	addi $sp, $sp, -16
-	sw $ra, 0($sp)
-	sw $s0, 4($sp)
-	sw $a0, 8($sp)
-	sw $a1, 12($sp)
-	beq $a0, $a1, equal   #see if n and k are equal
-	beq $a1, $zero, zero
+	addi $sp, $sp, -12     # adjust stack for 2 items    
+	sw   $ra, 0($sp)      # save return address    
+	sw   $a2, 4($sp)      # save argument
+	sw   $a1, 8($sp)
 	
-	
-	addi $t0, $t0, -1
-	addi $t1, $t1, -1
-	jal combo
-	addi $t0, $t0, -1
-	jal combo
-
-	li $v0, 1 
-	add $a0, $zero, $v1
-	syscall
-
-	lw $ra, 0($sp)
-	lw $s0, 4($sp)
-	addi $sp, $sp, 16
-	jr $ra
-	
-zero:
-	addi $v1, $v1, 1
-	
-	jr $ra
-equal:
+	bne $a2, $a1, L1   #see if n and k are equal
 	
 	li $v1, 1
 	jr $ra
 	
-fact:  
-	addi $sp, $sp, -8     # adjust stack for 2 items    
-	sw   $ra, 4($sp)      # save return address    
-	sw   $a0, 0($sp)      # save argument    
-	slti $t0, $a0, 1      # test for n < 1    
-	beq  $t0, $zero, L1    
-	addi $v1, $zero, 1    # if so, result is 1    
-	addi $sp, $sp, 8      #   pop 2 items from stack    
-	jr   $ra              #   and return
+	
 L1:
-	addi $a0, $a0, -1     # else decrement n      
-	jal  fact             # recursive call    
-	lw   $a0, 0($sp)      # restore original n    
-	lw   $ra, 4($sp)      #   and return address    
-	addi $sp, $sp, 8      # pop 2 items from stack    
-	mul  $v1, $a0, $v1    # multiply to get result    
-	jr   $ra              # and return
+	li $v0, 4
+	la $a0, debug
+	syscall
+	beq $a1, $zero, skip
+	
+	addi $a2, $a2, -1
+	addi $a1, $a1, -1
+	jal combo
+	addi $a2, $a2, -1
+	jal combo
+	lw $ra, 0($sp)
+	lw   $a2, 4($sp)
+	lw   $a1, 8($sp)
+	
+	jr $ra
+skip: 
+	lw $ra, 0($sp)
+	li $v1, 1
+	jr $ra
