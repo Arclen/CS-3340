64741f086364cab2b05a679e6ff36c871439f628
Jacob Liou <jacob.c.liou@gmail.com>
2017-03-09 17:48:10 -0600

    Update HW4.asm

--- HW4.asm
+++ HW4.asm
@@ -1,19 +1,42 @@
+.data
+	promptn: .asciiz "\nEnter the value of n: "
 .text
+	# Prompt the user to enter n
+	li $v0, 4
+	la $a0, promptn
+	syscall
+	# Get n
+	li $v0, 5
+	syscall
+	move $t0, $v0 # Store n into $t0
+	move $a1, $t0
 	
+	jal root
+	
+	li $v0, 1
+	move $a0, $v1
+	syscall
+	
+	li $v0,10 		# End program
+       	syscall
+       	
+       	
 	root:
 		li $t0, 1 # sum
 		li $t2, 1 # i
 		li $t1, 1 # N 
 	loop:
-		beq $a0, $t0, eq
-		blt $a0, $t0, grt
+		beq $a1, $t0, eq
+		blt $a1, $t0, grt
 		addi $t2, $t2, 2
 		add $t0, $t0, $t2
 		addi $t1, $t1, 1
+		j loop
 		
 	eq: 	
-		move $v0, $t1
+		move $v1, $t1
 		jr $ra
 	grt:
-		subi $t1, $t1, -1
-		move $v0, $t1
+		addi $t1, $t1, -1
+		move $v1, $t1
+		jr $ra
