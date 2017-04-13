f8c76166d07a61227a7b2d7c977126b9e78ebaf5
Jacob Liou <jacob.c.liou@gmail.com>
2017-04-13 17:02:42 -0500

    Set up loop

--- JALIp4.asm
+++ JALIp4.asm
@@ -1,9 +1,18 @@
 .data
-debug: .asciiz ":\t placeholder"
+debug: .asciiz ":\t placeholder\n"
 .text
 li $t0, 21
-la $a1, 4194304
-lw $t1, 0($a1)
+la $a1, 0x00400000 #or 4194304
+#lw $t1, 0($a1)
 add $a0, $a0, $t1
+li $v0, 1
+syscall
+
+loop:
+
+addi $a1, $a1, 4
+la $a0, debug
 li $v0, 4
 syscall
+
+j loop
