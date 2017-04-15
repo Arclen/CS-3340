6131ee9147e5f702691db83e3df611b5a90a5539
Jacob Liou <jacob.c.liou@gmail.com>
2017-04-15 13:27:29 -0500

    why am i in this major when i get absolutely no joy out of any of it

--- JALIp4.asm
+++ JALIp4.asm
@@ -1,18 +1,25 @@
 .data
-tab: .asciiz ":\t "
+tab: .asciiz ":\t opcode:\t"
 newline: .asciiz "\n"
-opcodes: .ascii "add  ","addi ", "addiu" "\n" 
+opcodes: .word addop, addiop, addiuop
+addop: .ascii "add "
+addiop: .ascii "addi "
+addiuop: .ascii "addiu"
+
 .text
 addi $t0, $t0, 21
 la $a1, 0x00400000
 lw $t1, 0($a1)
 
-
+la $a0, opcodes
+#lb $a0, ($s0)
+li $v0, 4
+syscall
 srl $t1, $t1, 26
 sll $t1, $t1, 26
 add $a0, $zero, $t1
 li $v0, 35
-syscall
+#syscall
 
 lw $t1, 4($a1)
 srl $t1, $t1, 26
@@ -38,7 +45,14 @@ li $v0, 4
 syscall
 la $a0, opcodes
 li $v0, 4
+#syscall
+
+lw $t1, 0($a1)
+srl $t1, $t1, 26
+add $a0, $zero, $t1
+li $v0, 35
 syscall
+
 la $a0, newline
 li $v0, 4
 syscall
