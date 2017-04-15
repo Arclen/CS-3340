e1ec686606ac4d5f1abb9d6a53c2455a6a3a2d70
Jacob Liou <jacob.c.liou@gmail.com>
2017-04-15 11:00:51 -0500

    Update JALIp4.asm

--- JALIp4.asm
+++ JALIp4.asm
@@ -5,7 +5,13 @@ addi $t0, $t0, 21
 la $a1, 0x00400000
 lw $t1, 0($a1)
 
-srl $t1,$t1, 26
+srl $t1, $t1, 26
+add $a0, $a0, $t1
+li $v0, 1
+syscall
+
+lw $t1, 4($a1)
+srl $t1, $t1, 26
 add $a0, $a0, $t1
 li $v0, 1
 syscall
@@ -18,11 +24,11 @@ loop:
 #beq something
 la $a0, debug
 li $v0, 4
-syscall
+#syscall
 add $a0, $a0, $a1
 #la $a0, addi
 li $v0, 34
-syscall
+#syscall
 addi $a1, $a1, 4
 
-j loop
+#j loop
