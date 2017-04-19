eab84e61f8a64e7ca41d34aa28354e1ffaff66b9
Jacob Liou <jacob.c.liou@gmail.com>
2017-04-18 20:34:15 -0500

    Add files via upload

---
+++ PageTest.asm
@@ -0,0 +1,16 @@
+.text
+li $a1, 0x00400000
+loop:
+lw $t1, 0($a1)
+addi $t2, $t1, -32
+beq $t2, $0, fin
+add $a0, $0, $t1
+li $v0, 1
+syscall
+addi $a0, $0, 0xA
+addi $v0, $0, 0xB 
+syscall
+addi $a1, $a1, 4
+j loop
+add $0,$0,$0
+fin:
